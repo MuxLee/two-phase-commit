1021243e8f623db0f7c148af0dfeb319e2f7050f
MuxLee <dnfl7080@naver.com>
2025-07-09 00:09:48 +0900

    [feat]: `Entity` 매니저 및 트랜잭션 `Bean` 추가 - 각 데이터베이스 별로 생성한 `Entity`들을 영속적으로 관리하는 `EntityManager`를 생성을 위한 `LocalContainerEntityManagerFactory` `Bean` 추가 - `Atomikos`로 생성된 `XADataSource`들의 트랜잭션을 관리하기 위한 `UserTransactionManager`와 `JtaTransactionManager` `Bean` 추가

--- src/main/java/net/mux/twophasecommit/database/config/AbstractDatabaseConfiguration.java
+++ src/main/java/net/mux/twophasecommit/database/config/AbstractDatabaseConfiguration.java
@@ -1,48 +1,44 @@
 package net.mux.twophasecommit.database.config;
 
+import com.atomikos.icatch.jta.UserTransactionManager;
+import jakarta.transaction.SystemException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.lang.NonNull;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.jta.JtaTransactionManager;
 
 import javax.sql.DataSource;
 
 abstract class AbstractDatabaseConfiguration implements ApplicationListener<ApplicationReadyEvent> {
 
-    protected static final Logger LOGGER;
+    protected final Logger logger;
 
-    private static final String DATASOURCE_BEAN_SUFFIX;
-
-    static {
-        DATASOURCE_BEAN_SUFFIX = "DataSource";
-        LOGGER = LoggerFactory.getLogger(AbstractDatabaseConfiguration.class);
+    protected AbstractDatabaseConfiguration() {
+        this.logger = LoggerFactory.getLogger(AbstractDatabaseConfiguration.class);
     }
 
     @Override
     public void onApplicationEvent(@NonNull final ApplicationReadyEvent applicationReadyEvent) {
         final var applicationContext = applicationReadyEvent.getApplicationContext();
+        final var listableBeanFactory = applicationContext.getBeanFactory();
 
         try {
-            final var appProperties = applicationContext.getBean(AppProperties.class);
-            final var beanFactory = applicationReadyEvent.getApplicationContext()
-                    .getBeanFactory();
-            final var databases = appProperties.getDatabases();
-
-            for (final var database: databases) {
-                final var dataSourceBeanName = database.getName() + DATASOURCE_BEAN_SUFFIX;
-                final var dataSource = this.createDataSource(dataSourceBeanName, database);
-
-                beanFactory.registerSingleton(dataSourceBeanName, dataSource);
-            }
+            this.registerDatabaseBean(applicationContext, listableBeanFactory);
+            this.registerTransactionBean(listableBeanFactory);
         }
         catch (NoSuchBeanDefinitionException ignored) {
-            LOGGER.warn("AppProperties bean을 찾을 수 없습니다");
+            logger.warn("AppProperties bean을 찾을 수 없습니다");
         }
         catch (Exception exception) {
-            LOGGER.error(exception.getMessage(), exception);
+            logger.error(exception.getMessage(), exception);
         }
     }
 
@@ -50,5 +46,47 @@ abstract class AbstractDatabaseConfiguration implements ApplicationListener<Appl
         @NonNull final String dataSourceBeanName,
         @NonNull final DataSourceProperties dataSourceProperties
     );
+
+    private void registerDatabaseBean(
+            @NonNull final ConfigurableApplicationContext applicationContext,
+            @NonNull final ConfigurableListableBeanFactory listableBeanFactory
+    ) throws NoSuchBeanDefinitionException {
+        final var appProperties = applicationContext.getBean(AppProperties.class);
+        final var databases = appProperties.getDatabases();
+
+        for (final var database: databases) {
+            final var databaseName = database.getName();
+            final var dataSourceBeanName = databaseName + "DataSource";
+            final var dataSource = this.createDataSource(dataSourceBeanName, database);
+
+            listableBeanFactory.registerSingleton(dataSourceBeanName, dataSource);
+
+            final var entityManagerFactoryBeanName = databaseName + "EntityManagerFactory";
+            final var entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+            final var jpaVendorAdapter = new HibernateJpaVendorAdapter();
+            final var scanningPackages = database.getScanningPackages();
+
+            entityManagerFactoryBean.setDataSource(dataSource);
+            entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+            entityManagerFactoryBean.setPackagesToScan(scanningPackages);
+            jpaVendorAdapter.setGenerateDdl(true);
+            listableBeanFactory.registerSingleton(entityManagerFactoryBeanName, entityManagerFactoryBean);
+        }
+    }
+
+    private void registerTransactionBean(@NonNull final ConfigurableListableBeanFactory listableBeanFactory)
+            throws SystemException {
+        final var userTransactionManager = new UserTransactionManager();
+
+        userTransactionManager.setForceShutdown(true);
+        userTransactionManager.setTransactionTimeout(500);
+        listableBeanFactory.registerSingleton("userTransactionManager", userTransactionManager);
+
+        final var jtaTransactionManager = new JtaTransactionManager();
+
+        jtaTransactionManager.setTransactionManager(userTransactionManager);
+        jtaTransactionManager.setUserTransaction(userTransactionManager);
+        listableBeanFactory.registerSingleton("jtaTransactionManager", jtaTransactionManager);
+    }
 
 }

17aca4faed5b453d8e047268ab268711a85beeee
MuxLee <dnfl7080@naver.com>
2025-07-18 00:03:17 +0900

    [feat]: `Spring` 맞춤 `TransactionManager` 추가 - `JTA`(Java Transaction API) 분산 트랜잭션 구현체 추가 - `Jakarta` 패키지에서 제공되는 `TransactionManager`, `UserTransactionManager`를 제공받을 수 있도록 처리

--- src/main/java/net/mux/twophasecommit/database/UserTransactionManagerFactory.java
+++ src/main/java/net/mux/twophasecommit/database/UserTransactionManagerFactory.java
@@ -3,72 +3,45 @@ package net.mux.twophasecommit.database;
 import jakarta.transaction.UserTransaction;
 import org.springframework.lang.NonNull;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
 public class UserTransactionManagerFactory implements TransactionManagerFactory {
 
-    private final Map<String, UserTransaction> transactionManagers;
+    private UserTransaction userTransaction;
+    private String userTransactionManagerName;
 
-    public UserTransactionManagerFactory() {
-        this.transactionManagers = new LimitedHashMap<>(1);
+    public UserTransactionManagerFactory(
+            @NonNull final UserTransaction userTransaction,
+            @NonNull final String userTransactionManagerName
+    ) {
+        this.userTransaction = userTransaction;
+        this.userTransactionManagerName = userTransactionManagerName;
     }
 
     @NonNull
     @Override
     public Map<String, ?> getTransactionManagers() {
-        if (this.transactionManagers.isEmpty()) {
-            throw new IllegalStateException("No transactionManager found.");
+        if (this.userTransaction == null) {
+            throw new IllegalArgumentException("'userTransaction' must not be null");
         }
 
-        return Collections.unmodifiableMap(this.transactionManagers);
+        if (this.userTransactionManagerName == null) {
+            throw new IllegalArgumentException("'userTransactionManagerName' must not be null");
+        }
+
+        if (this.userTransactionManagerName.isBlank()) {
+            throw new IllegalArgumentException("'userTransactionManagerName' must not be blank");
+        }
+
+        return Map.of(this.userTransactionManagerName, this.userTransaction);
     }
 
-    public void setTransactionManager(
-            @NonNull final String userTransactionName,
-            @NonNull final UserTransaction userTransaction
-    ) {
-        if (userTransactionName == null) {
-            throw new IllegalArgumentException("'userTransactionName' must not be null.");
-        }
-
-        if (userTransactionName.isBlank()) {
-            throw new IllegalArgumentException("'userTransactionName' must not be blank.");
-        }
-
-        if (userTransaction == null) {
-            throw new IllegalArgumentException("'userTransaction' must not be null.");
-        }
-
-        if (!this.transactionManagers.isEmpty()) {
-            this.transactionManagers.clear();
-        }
-
-        this.transactionManagers.put(userTransactionName, userTransaction);
+    public void setUserTransaction(@NonNull final UserTransaction userTransaction) {
+        this.userTransaction = userTransaction;
     }
 
-    private static class LimitedHashMap<K, V> extends HashMap<K, V> {
-
-        private final int limit;
-
-        private LimitedHashMap(final int limit) {
-            this.limit = limit;
-        }
-
-        @Override
-        public V put(final K key, final V value) {
-            if (super.containsKey(key) || this.isOverLimit()) {
-                return super.put(key, value);
-            }
-
-            throw new IllegalStateException("It exceeded the set limit size of " + this.limit + ".");
-        }
-
-        public boolean isOverLimit() {
-            return this.size() >= this.limit;
-        }
-
+    public void setUserTransactionManagerName(@NonNull final String userTransactionManagerName) {
+        this.userTransactionManagerName = userTransactionManagerName;
     }
 
 }
